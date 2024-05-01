9786efad77ee9c85d21ddabce52d13e7d1b3b493
Maksym_Nemera <81112084+maksym-nemera@users.noreply.github.com>
2024-05-01 18:22:02 +0300

    Initial commit

---
+++ src/scripts/main.js
@@ -0,0 +1,12 @@
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import MicroModal from 'micromodal';
+
+// eslint-disable-next-line no-console
+console.log('hello fron js');
+
+AOS.init({
+  duration: 600,
+});
+
+MicroModal.init();
